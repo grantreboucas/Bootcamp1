13584af9f505cd6192fb52062e866f90d8707837
grantreboucas <grant.reboucas97@gmail.com>
2019-09-06 11:29:18 -0400

    I hope this works

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ var requestHandler = function(request, response) {
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
     is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
     HINT: Explore the request object and its properties 
     HINT: Explore the response object and its properties
     https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
@@ -24,28 +23,44 @@ var requestHandler = function(request, response) {
     HINT: Explore the list of MIME Types
     https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
    */
+  if(parsedUrl.pathname === '/listings')
+  {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.write(JSON.stringify(listingData));
+  }
+  else
+  {
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('Bad gateway error');
+  }
+  response.end();
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
   /*
     This callback function should save the data in the listingData variable, 
     then start the server. 
-
     HINT: Check out this resource on fs.readFile
     //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
-
     HINT: Read up on JSON parsing Node.js
    */
 
     //Check for errors
+    if (err)
+    {
+        throw err;
+    }
   
 
    //Save the sate in the listingData variable already defined
-  
+  listingData = JSON.parse(data);
 
   //Creates the server
+  server = http.createServer(requestHandler);
   
   //Start the server
+  server.listen(port);
+  console.log('server listening on http://localhost:' + port);
 
 
 });
